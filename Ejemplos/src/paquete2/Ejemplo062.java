a6391287eb76f51124e259cbc6228928d89cb79f
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-12-11 12:26:28 -0500

    Cambios

--- Ejemplos/src/paquete2/Ejemplo062.java
+++ Ejemplos/src/paquete2/Ejemplo062.java
@@ -5,13 +5,14 @@
  */
 package paquete2;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
  *
  * @author reroes
  */
-public class Ejemplo05 {
+public class Ejemplo062 {
 
     /**
      * @param args the command line arguments
@@ -19,16 +20,26 @@ public class Ejemplo05 {
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese el numero de elementos");
-        int numero_elementos = entrada.nextInt();
+        entrada.useLocale(Locale.US);
+        double dato;
         // arreglo de tipo entero
-        int[] arreglo1 = new int[numero_elementos];
-        // Se asigna valores al arreglo
+        double [] arreglo1 = new double[7];
         
-        System.out.printf("%d\n", arreglo1.length);
+        // Se asigna valores al arreglo, pidiendo valores por teclado
+        // se usa un ciclo repetitivo
+        for (int i = 0; i < arreglo1.length; i++) {
+        
+            System.out.println("Ingrese un valor para el arreglo");
+            dato = entrada.nextDouble();
+            // se asigna la variable dato a la posición del arreglo
+            // de acuerdo al valor de i
+            arreglo1[i] = dato;
+            
+        }
+        
         // Se hace uso del ciclo repetitivo for para recorrer el arreglo
         for (int i = 0; i < arreglo1.length; i++) {
-            System.out.printf("Subíndice o Índice %d, con valor %d\n",
+            System.out.printf("Subíndice o Índice %d, con valor %.2f\n",
                     i, arreglo1[i]);
         }
         
