20dc88afd4daed2e78448c0554322d93d9a6a2c4
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-12-11 11:45:47 -0500

    Cambios

--- Ejemplos/src/paquete1/Ejemplo02.java
+++ Ejemplos/src/paquete1/Ejemplo02.java
@@ -24,18 +24,25 @@ public class Ejemplo02 {
         System.out.println(arreglo1[2]);
         System.out.println(arreglo1[3]);
         System.out.println(arreglo1[4]);
+        //Inprime 0 porque no  tiene ningun valor asignado y es deo tipo
+        //entero
         
         // arreglo de tipo double
         double[] arreglo2 = new double[5];
         System.out.println(arreglo2[0]);
+        //Inprime 0.0 porque no tiene ningun valor asignado y es de tipo double
+        //(Decimal)
         
         // arreglo de tipo String
         String[] arreglo4 = new String[5];
         System.out.println(arreglo4[0]);
+        //Inprime null porque no tiene ningun valor asignado y es de tipo cadena
         
         // arreglo de tipo boleano
         boolean[] arreglo5 = new boolean[5];
         System.out.println(arreglo5[0]);
+        //Inprime false porque no tiene ningun valor asignado y es de tipo
+        //boleano
     }
 
 }
